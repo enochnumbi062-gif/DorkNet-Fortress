7b1722dd6a30ce083499960a62343fa754f27d08
DorkNet <enochnumbi062@gmail.com>
2026-02-09 16:00:42 +0100

    Update auth_engine.py

--- auth_engine.py
+++ auth_engine.py
@@ -1,22 +1,22 @@
+# Fichier : auth_engine.py
+import pyotp
 import secrets
 import string
-import pyotp
 from cryptography.fernet import Fernet
 
 class FortressSecurity:
     @staticmethod
     def generate_secure_password(length=24):
-        """Génère un mot de passe inviolable."""
+        """Génère un mot de passe robuste."""
         alphabet = string.ascii_letters + string.digits + "!@#$%^&*"
         return ''.join(secrets.choice(alphabet) for _ in range(length))
 
     @staticmethod
     def get_cipher(key):
-        """Initialise le moteur de chiffrement."""
+        """Initialise le moteur de chiffrement AES-256."""
         return Fernet(key)
 
     @staticmethod
     def encrypt_data(cipher, data):
-        """Chiffre les données binaires."""
+        """Chiffre les données binaires avant stockage."""
         return cipher.encrypt(data)
-      
